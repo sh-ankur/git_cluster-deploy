0a0ab8a85e14c381bff70dff2070836c6fbb9202
Ankur Sharma <ankur.sharma@bigdata.uni-saarland.de>
2019-05-23 15:40:51 +0200

    simple script to deploy tendermint on the hail cluster automatically

---
+++ README.md
@@ -0,0 +1,4 @@
+## Deploy tendermint using Git Post-Receive Hook
+
+This repository hosts the script which can be used to deploy tendermint cluster on multiple nodes using git post-receive hook.
+
---
+++ deploy.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# deploy tendermint on our cluster
+
+LOCAL_DEPLOY_REPO_PATH=$HOME/tendermint
+TENDERMINT_BUILD_DIR=$GOPATH/src/github.com/tendermint
+
+# create required directories
+
+rm -rf ${TENDERMINT_BUILD_DIR}
+rm -rf ${LOCAL_DEPLOY_REPO_PATH}
+
+mkdir -p ${TENDERMINT_BUILD_DIR}
+mkdir -p ${LOCAL_DEPLOY_REPO_PATH}
+
+cd ${LOCAL_DEPLOY_REPO_PATH}
+git init --bare
+
+# create the post-receive hook
+cd hooks || exit
+
+tee post-receive <<EOF
+#!/bin/bash
+
+export GOBIN=/var/nfs/gobin
+export PATH=$PATH:$GOBIN
+
+# tendermint build directory
+TENDERMINT_BUILD_DIR=${TENDERMINT_BUILD_DIR}
+LOCAL_DEPLOY_REPO_PATH=${LOCAL_DEPLOY_REPO_PATH}
+
+cd \${TENDERMINT_BUILD_DIR}
+
+unset GIT_DIR
+
+# delete the existing directory
+rm -rf tendermint
+git clone ${LOCAL_DEPLOY_REPO_PATH}
+
+# build tendermint
+cd tendermint || exit
+make get_tools && make get_vendor_deps
+make && make install
+
+# copy the config files to nfs
+rm -rf /var/nfs/tendermint_config
+cp -a tendermint_config /var/nfs/
+
+# restart the tendermint cluster
+./cluster.sh restart
+
+EOF
+
+# make the post-receive hook executable
+chmod +x post-receive
